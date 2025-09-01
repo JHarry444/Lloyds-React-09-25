72ecfb2f84f7fe3d9379d7c6d473c4556941cab8
jharry444 <jharrison1@qa.com>
2025-09-01 15:13:32 +0100

    conditional rendering

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,19 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 import Trainer from './components/Trainer'
 import Input from './components/Input'
+import me from "./assets/me.jpg";
 
 function App() {
 
   return (
     <>
-      <Trainer />
-      <Input/>
+      <img src={me} alt="cheeky chappy" />
+      <Trainer name={"Jordan"} age={31} specialism={"Java"} picture={"https://avatars.githubusercontent.com/u/41322826?v=4"} />
+      <Trainer name={"Cameron"} age={32} specialism={"Web"} />
+      {
+        Trainer({name: "Shafeeq", age: 31, specialism: "Oracle"})
+      }
+      <Input />
       <input type="text" />
     </>
   )
--- src/components/Trainer.jsx
+++ src/components/Trainer.jsx
@@ -1,9 +1,45 @@
-export default function Trainer() {
-    return (
+export default function Trainer({ name, age, specialism, picture }) {
+    console.log("PROPS:", { name, age, specialism });
+
+    // if (!picture) {
+    //     return (
+    //         <>
+    //             <h2>Trainer w/o pic</h2>
+    //             <p>Name: {name}</p>
+    //             <p>Age: {age}</p>
+    //             <p>Specialism: {specialism}</p>
+    //         </>
+    //     )
+    // } else {
+    //     return (
+    //         <>
+    //             <h2>Trainer w/ pic</h2>
+    //             <p>Name: {name}</p>
+    //             <p>Age: {age}</p>
+    //             <p>Specialism: {specialism}</p>
+    //             <img src={picture} width={"50px"} />
+    //         </>
+    //     )
+    // }
+
+    // return (
+    //     <>
+    //         <p>Name: {name}</p>
+    //         <p>Age: {age}</p>
+    //         <p>Specialism: {specialism}</p>
+    //         {picture ? <img src={picture} width={"50px"} /> : false}
+    //     </>
+    // )
+
+        return (
         <>
-            <p>Name: Jordan</p>
-            <p>Age: 31</p>
-            <p>Specialism: Java</p>
+            <p>Name: {name}</p>
+            <p>Age: {age}</p>
+            <p>Specialism: {specialism}</p>
+            {picture && <img src={picture} width={"50px"} />}
         </>
     )
 }
+
+
+// `Hello, my name is ${name}`
