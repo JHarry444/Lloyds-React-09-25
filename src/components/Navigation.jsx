8c4bd717dd0de93cd46ae2af400f784e690ee417
jordan h <jharrison1@qa.com>
2025-09-02 11:33:25 +0100

    neatened the navbar

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -2,9 +2,9 @@ import { Link } from "react-router";
 
 export default function Navigation() {
     return(
-        <>
+        <nav>
             <Link to="/">Home</Link>
             <Link to="/props">Props</Link>
-        </>
+        </nav>
     )
 }
