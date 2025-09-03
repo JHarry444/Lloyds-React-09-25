12668ca453053feaa8c1f178d2a326a85069ae79
jordan h <jharrison1@qa.com>
2025-09-03 09:08:09 +0100

    extra links

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -1,10 +1,16 @@
 import { Link } from "react-router";
 
 export default function Navigation() {
-    return(
+    return (
         <nav>
             <Link to="/">Home</Link>
+            <Link to="/images">Images</Link>
+            <Link to="/components">Components</Link>
             <Link to="/props">Props</Link>
+            <Link to="/external">External Data</Link>
+            <Link to="/params/:name">Route Params</Link>
+            <Link to="/search">Search Params</Link>
+
         </nav>
     )
 }
