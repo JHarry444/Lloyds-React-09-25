c647a8b79c825aad2731bc0d1aed124623982409
jharry444 <jharrison1@qa.com>
2025-09-03 14:29:53 +0100

    useEffect

--- src/components/interactive/lifting_state/TrainerContainer.jsx
+++ src/components/interactive/lifting_state/TrainerContainer.jsx
@@ -1,23 +1,30 @@
 import './lifting.css';
-import { useState } from "react"
+import { useEffect, useState } from "react"
 import TrainerForm from "./TrainerForm";
 import TrainerList from "./TrainerList";
 
 export default function TrainerContainer() {
-    const [trainers, setTrainers] = useState([
-        {
-            name: 'JH',
-            age: 31,
-            specialism: 'Java'
-        }
-    ]);
+    const [trainers, setTrainers] = useState([]);
 
     console.log("TRAINERS:", trainers);
-    
+
+    // onload/onmount
+
+    useEffect(function () {
+        fetch("http://localhost:8080/trainers")
+            .then(response => response.json())
+            .then(json => {
+                setTrainers(json)
+            })
+            .catch(err => console.error(err))
+    }, []);
+
+
+
     return (
         <div className="trainerContainer">
-            <TrainerForm setTrainers={setTrainers}/>
-            <TrainerList trainers={trainers}/>
+            <TrainerForm setTrainers={setTrainers} />
+            <TrainerList trainers={trainers} />
         </div>
     )
 }
